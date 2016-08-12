f200d31dc073cb68a495f6fd27e7e69ec4a4ac8c
Drunkar <drunkars.p@gmail.com>
2016-08-12 17:43:58 +0900

    Fix syntax error.

--- test_build_all.py
+++ test_build_all.py
@@ -7,7 +7,7 @@ import commands
 CMD = "openscad -o ${CIRCLE_ARTIFACTS}/"
 GREP = " | egrep -c 'ERROR|WARNING'"
 
-for file in glob("modules/*.scad")
+for file in glob("modules/*.scad"):
     output_file = os.path.splitext(os.path.basename(file))[0]
     function = commands.getoutput(CMD + output_file + ".stl modules/" + file + GREP)
     expect = "0"
