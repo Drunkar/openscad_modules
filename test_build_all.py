ac1e5e4e8e51068c23cd5441036545dea4a48222
Drunkar <drunkars.p@gmail.com>
2016-08-12 17:19:50 +0900

    Assert test.

--- test_build_all.py
+++ test_build_all.py
@@ -5,4 +5,4 @@ import commands
 CMD = "openscad -o ${CIRCLE_ARTIFACTS}/ArcOfCylinder.stl "
 GREP = " | egrep -c 'ERROR|WARNING'"
 
-assert getoutput(CMD + "modules/ArcOfCylinder.scad" + GREP) == "0"
+assert commands.getoutput(CMD + "modules/ArcOfCylinder.scad" + GREP) == "0"
