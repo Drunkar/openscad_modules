5aa2c1d9f4477e58e7c9639cc213d5e507ee7ec2
Drunkar <drunkars.p@gmail.com>
2016-08-12 17:40:26 +0900

    Implement build all test.

--- test_build_all.py
+++ test_build_all.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
+import os
+from glob import glob
 import commands
 
-CMD = "openscad -o ${CIRCLE_ARTIFACTS}/ArcOfCylinder.stl "
+CMD = "openscad -o ${CIRCLE_ARTIFACTS}/"
 GREP = " | egrep -c 'ERROR|WARNING'"
 
-function = commands.getoutput(CMD + "modules/ArcOfCylinder.scad" + GREP)
-expect = "0"
-try:
-    assert function == expect
-except AssertionError:
-    print("Expect " + str(expect) + ", but the result was " + str(function) + ".")
+for file in glob("modules/*.scad")
+    output_file = os.path.splitext(os.path.basename(file))[0]
+    function = commands.getoutput(CMD + output_file + ".stl modules/" + file + GREP)
+    expect = "0"
+    try:
+        assert function == expect
+    except AssertionError:
+        print("Expect " + str(expect) + ", but the result was " + str(function) + ".")
