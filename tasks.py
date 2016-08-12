473aa7216f00c5598d3effd18e85f33c808cf300
Drunkar <drunkars.p@gmail.com>
2016-08-12 17:03:36 +0900

    Assert test.

--- tasks.py
+++ tasks.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
-
 import invoke
 
+CMD = "openscad -o ${CIRCLE_ARTIFACTS}/ArcOfCylinder.stl "
+GREP = " | egrep -c 'ERROR|WARNING'"
 
 @invoke.task
 def test(ctx):
-    invoke.run("openscad -o ${CIRCLE_ARTIFACTS}/ArcOfCylinder.stl modules/ArcOfCylinder.scad | egrep -c 'ERROR|WARNING'")
+    assert invoke.run(CMD + "modules/ArcOfCylinder.scad" + GREP) == 0
+    return
