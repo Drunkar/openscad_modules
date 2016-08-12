759d448cbf9927554ccf968a7d32de0b46aed8fe
Drunkar <drunkars.p@gmail.com>
2016-08-12 17:08:12 +0900

    Assert test.

--- tasks.py
+++ tasks.py
@@ -7,5 +7,5 @@ GREP = " | egrep -c 'ERROR|WARNING'"
 
 @invoke.task
 def test(ctx):
-    assert invoke.run(CMD + "modules/ArcOfCylinder.scad" + GREP) == 0
+    assert invoke.run(CMD + "modules/ArcOfCylinder.scad" + GREP) == "0"
     return
