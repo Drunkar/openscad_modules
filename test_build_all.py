9d73a88077ff87c15f788772591a184822d5147d
Drunkar <drunkars.p@gmail.com>
2016-08-24 02:02:39 +0900

    Warning test.

--- test_build_all.py
+++ test_build_all.py
@@ -10,21 +10,18 @@ OUTPUT_PATH = os.environ["CIRCLE_ARTIFACTS"]
 
 def test_build_all():
     for file in glob("modules/*.scad"):
-        print("----------------------------------------------------")
+        print("--------------------------------------------------------------")
         print(file)
         output_file = os.path.splitext(os.path.basename(file))[0]
         p = subprocess.Popen(["openscad", "-o", OUTPUT_PATH + "/" + output_file + ".stl", file],
-                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        function_stdout, function_stderr = stdout_data, stderr_data = p.communicate()
-        error_count = function_stdout.count(b"ERROR")
-        warning_count = function_stdout.count(b"WARNING")
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        error_count = out.count(b"ERROR") + err.count(b"ERROR")
+        warning_count = err.count(b"WARNING") + out.count(b"WARNING")
         expect = 0
-        print(function_stderr)
-        print(function_stdout)
-        print(error_count)
-        print(warning_count)
+        print(out)
+        print(error)
         try:
             assert error_count + warning_count == expect
         except AssertionError:
-            print(function_stdout)
             raise Exception("ERROR or WARNING exists!")
