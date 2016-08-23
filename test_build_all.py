16dce79c1f832b14e123994b51d1f0e2d5867af2
Drunkar <drunkars.p@gmail.com>
2016-08-24 01:55:06 +0900

    Warning test.

--- test_build_all.py
+++ test_build_all.py
@@ -13,11 +13,13 @@ def test_build_all():
         print("----------------------------------------------------")
         print(file)
         output_file = os.path.splitext(os.path.basename(file))[0]
-        function_stdout = subprocess.run(
-            ["openscad", "-o", OUTPUT_PATH + "/" + output_file + ".stl", file], stdout=subprocess.PIPE).stdout
+        p = subprocess.run(["openscad", "-o", OUTPUT_PATH + "/" + output_file + ".stl", file],
+                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        function_stdout, function_stderr = stdout_data, stderr_data = p.communicate()
         error_count = function_stdout.count(b"ERROR")
         warning_count = function_stdout.count(b"WARNING")
         expect = 0
+        print(function_stderr)
         print(function_stdout)
         print(error_count)
         print(warning_count)
