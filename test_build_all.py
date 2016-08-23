82cbdde46552384f09b815a19a6bc38583f642c2
Drunkar <drunkars.p@gmail.com>
2016-08-24 01:37:17 +0900

    Warning test.

--- test_build_all.py
+++ test_build_all.py
@@ -9,12 +9,16 @@ OUTPUT_PATH = os.environ["CIRCLE_ARTIFACTS"]
 
 def test_build_all():
     for file in glob("modules/*.scad"):
+        print("----------------------------------------------------")
+        print(file)
         output_file = os.path.splitext(os.path.basename(file))[0]
         function_stdout = subprocess.check_output(
             ["openscad", "-o", OUTPUT_PATH + "/" + output_file + ".stl", file])
         error_count = function_stdout.count(b"ERROR")
         warning_count = function_stdout.count(b"WARNING")
         expect = 0
+        print(error_count)
+        print(warning_count)
         try:
             assert error_count + warning_count == expect
         except AssertionError:
